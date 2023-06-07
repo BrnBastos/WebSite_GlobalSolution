688ac52c492d76987a39e4c18845ca87fb7269dd
BrnBastos <rm550416@fiap.com.br>
2023-06-07 16:03:49 -0300

    add login do Admin 123456 e do Usuario 123456

--- JS/login.js
+++ JS/login.js
@@ -3,8 +3,12 @@ function login() {
     var password = document.getElementById('password').value;
     
     if (username === 'Admin' && password === '123456') {
+       window.location.href = 'https://www.unicef.org/brazil/comunicados-de-imprensa/relatorio-da-onu-numeros-globais-de-fome-subiram-para-cerca-de-828-milhoes-em-2021';
+    } 
+    else if (username === 'Usuario' && password === '123456') {
         localStorage.setItem('logado', 'true');
         window.location.href = 'index.html';
+    
     } else {
         document.getElementById('error-message').textContent = 'Usuário ou senha inválidos';
     }
